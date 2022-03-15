3b941d659838381f0a115085b99ef30d1ad62967
Jakub Dvořák <dvorakja.04@spst.cz>
2022-03-15 17:41:39 +0100

    Přidány Metody12 -vrátí zda řetězec v textbox1 obsahuje slovo -když ano, tak také vrátí nejkratší a nejdelší slovo řetězce

---
+++ Metody12/Metody12/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool ObsahujeSlovo (string retezec, out string nejdelsiSlovo, out string nejkratsiSlovo)
+        {
+            bool obsahujeSlovo = false;
+            string[] separator = { " " };
+            string[] slova = retezec.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            nejdelsiSlovo = "xxx";
+            nejkratsiSlovo = "xxx";
+
+            if (slova.Length > 0)
+            {
+                obsahujeSlovo = true;
+                nejdelsiSlovo = slova[0];
+                nejkratsiSlovo = slova[0];
+                foreach (string slovo in slova)
+                {
+                    if (slovo.Length > nejdelsiSlovo.Length) nejdelsiSlovo = slovo;
+                    if (slovo.Length < nejkratsiSlovo.Length) nejkratsiSlovo = slovo;
+                }
+            }
+
+            return obsahujeSlovo;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string nejdelsiSlovo, nejkratsiSlovo;
+
+            if (ObsahujeSlovo(s, out nejdelsiSlovo, out nejkratsiSlovo)) MessageBox.Show("Ano, daný řetězec obsahuje alespoň jedno slovo" + "\nNejdelší slovo v řetezci je: " + nejdelsiSlovo
+                + "\nNejkratší slovo v řetězci je: " + nejkratsiSlovo);
+            else MessageBox.Show("Ne, řetězec neobsahuje žádné slovo");
+        }
+    }
+}
