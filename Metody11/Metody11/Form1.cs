5bbb46093c390a546bb473c1de3b0d631b750f65
Jakub Dvořák <dvorakja.04@spst.cz>
2022-03-15 17:23:40 +0100

    Přidány Metody11 -vrátí počet slov v řetězci textBox1 a smaže všechny cifry

---
+++ Metody11/Metody11/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int PocetSlov(string retezec, out string vyslednyRetezec)
+        {
+            char[] separator = { ' ' };
+            string[] slova = retezec.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            int pocetSlov = slova.Length;
+
+            int i = 0;
+            while (i < retezec.Length)
+            { 
+                if (Char.IsDigit(retezec[i])) retezec = retezec.Remove(i, 1);
+                else ++i;
+            }
+
+            vyslednyRetezec = retezec;
+            return pocetSlov;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string s2;
+
+            MessageBox.Show("Počet slov v řetězci je: " + PocetSlov(s, out s2) + "\nUpravený řezězec vypadá takto: " + s2);
+        }
+    }
+}
