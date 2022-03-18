90775ec69433a561f53a1899cae2286273cfeb28
Dvorakja.04 <Dvorakja.04@A321-14>
2022-03-18 10:30:31 +0100

    Dokončeny Metody13 -zjistí, zda daný řetězec je alfanumerický -vrátí počet velkých a malých písmen -vrátí počet znaků jiných než alfanumerických

--- Metody13/Metody13/Form1.cs
+++ Metody13/Metody13/Form1.cs
@@ -16,5 +16,37 @@ namespace Metody13
         {
             InitializeComponent();
         }
+
+        private bool JeAlfanum (string retezec, out int pocetMalPis, out int pocetVelPis, out int pocetJinych)
+        {
+            bool jeAlfanum = true;
+            pocetMalPis = 0;
+            pocetVelPis = 0;
+            pocetJinych = 0;
+
+            for (int i = 0; i < retezec.Length; ++i)
+            {
+                if (char.IsLetterOrDigit(retezec[i]))
+                {
+                    if (char.IsUpper(retezec[i])) ++pocetVelPis;
+                    else if (char.IsLower(retezec[i])) ++pocetMalPis;
+                }
+                else ++pocetJinych;                
+            }
+
+            if (pocetJinych > 0) jeAlfanum = false;
+            return jeAlfanum;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            int pocet1 = 0, pocet2 = 0, pocet3 = 0;
+
+            if (JeAlfanum(s, out pocet1, out pocet2, out pocet3)) MessageBox.Show("Ano, daný řetězec obsahuje pouze písmena a cifry\n" + "Počet malých čísel v řetězci je: " + pocet1
+                + "\nPočet velkých písmen je: " + pocet2 + "\nPočet jiných znaků než alfanumerických je: " + pocet3);
+            else MessageBox.Show("Ne, daný řetězec neobsahuje pouze písmena a cifry\n" + "Počet malých čísel v řetězci je: " + pocet1 + "\nPočet velkých písmen je: " + pocet2
+                + "\nPočet jiných znaků než alfanumerických je: " + pocet3);
+        }
     }
 }
